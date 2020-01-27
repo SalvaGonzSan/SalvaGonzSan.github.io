c80f3924ee003f769e9fd10fd9188446796ece2b
Salvador.Gonzalez <salvador.gonzalez@infomm.local>
2020-01-27 20:09:57 +0100

    Version 1.0

---
+++ precache-manifest.aff62a9799026f0635b4afeb8af5ebcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb6e998e2687e55df39b66acdf83c9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef315684a503944698c2",
+    "url": "./static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "f41f09a282cfcd6fab21",
+    "url": "./static/js/2.7da0e140.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.7da0e140.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef315684a503944698c2",
+    "url": "./static/js/main.a141978a.chunk.js"
+  },
+  {
+    "revision": "f8b06b5376d5363995bf",
+    "url": "./static/js/runtime-main.adb1065d.js"
+  }
+]);
